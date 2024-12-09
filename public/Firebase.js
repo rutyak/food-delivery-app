36ffe5f6230c36b6de0f87de966f501c0a1ae90d
rutyak <rutikkhandekar123@gmail.com>
2024-12-10 00:14:47 +0530

    nne-changes

--- public/Firebase.js
+++ public/Firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
-import firebaseConfig from "../Config/firebaseConfig";
+import firebaseConfig from "../Config/FirebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
